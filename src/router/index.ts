33f08d019f46e50ffac21d6bcecf8b0dff4081f4
CharlieYouu <99578012+CharlieYouu@users.noreply.github.com>
2023-06-24 02:09:40 +0900

    add login

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,21 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Login from '../views/login/login.vue'
+import Login from '../views/login/Login.vue'
+import Main from '../views/main/Main.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'login',
+      name: 'Login',
       component: Login
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/Main',
+      name: 'Main',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: Main
     }
   ]
 })
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,9 +1,65 @@
 <template>
-  <div class="about">
-    <h1>login</h1>
+  <div class="login">
+    <a-form
+      :model="formState"
+      name="basic"
+      :label-col="{ span: 8 }"
+      :wrapper-col="{ span: 16 }"
+      autocomplete="off"
+      @finish="onFinish"
+      @finishFailed="onFinishFailed"
+    >
+      <a-form-item
+        label="Username"
+        name="username"
+        :rules="[{ required: true, message: 'Please input your username!' }]"
+      >
+        <a-input v-model:value="formState.username" />
+      </a-form-item>
+
+      <a-form-item
+        label="Password"
+        name="password"
+        :rules="[{ required: true, message: 'Please input your password!' }]"
+      >
+        <a-input-password v-model:value="formState.password" />
+      </a-form-item>
+
+      <a-form-item :wrapper-col="{ offset: 8, span: 16 }">
+        <a-button type="primary" html-type="submit">Login</a-button>
+      </a-form-item>
+    </a-form>
   </div>
 </template>
 
-<style>
+<script lang="ts">
+import { defineComponent, reactive } from 'vue'
 
-</style>
+interface FormState {
+  username: string
+  password: string
+}
+
+export default defineComponent({
+  setup() {
+    const formState = reactive<FormState>({
+      username: '',
+      password: ''
+    })
+
+    const onFinish = (values: any) => {
+      console.log('Success:', values)
+    }
+
+    const onFinishFailed = (errorInfo: any) => {
+      console.log('Failed:', errorInfo)
+    }
+
+    return {
+      formState,
+      onFinish,
+      onFinishFailed
+    }
+  }
+})
+</script>
